0076a25630462c31b945e52134ecd796ec65e0ef
Kiran Garimella <kiran@broadinstitute.org>
2025-05-30 23:14:28 -0700

    Build both Python and Rust documentation

---
+++ docs/source/index.rst
@@ -0,0 +1,18 @@
+Welcome to Storm's documentation!
+================================
+
+Storm is a high-performance data processing framework that provides both Python and Rust APIs.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   python/index
+   rust/index
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search` 
--- python/storm/__init__.py
+++ python/storm/__init__.py
@@ -1,5 +1,16 @@
+"""Storm - A high-performance data processing framework.
+
+This package provides Python bindings for the Storm data processing framework.
+Storm is designed for high-performance data processing and analysis tasks.
+"""
+
 import storm.storm as storm  # noqa: F403
 
 
-def version():
+def version() -> str:
+    """Get the current version of the Storm package.
+
+    Returns:
+        str: The version string of the currently installed Storm package.
+    """
     return storm._version()
